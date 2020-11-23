5be4d3a5eba93a7b86356991b1d06b225c897a5f
Liuba Kalyta <liuba.kalyta@gmail.com>
2020-11-23 19:38:22 +0200

    add test class

---
+++ src/App/Test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Description of Test.php
+ * @copyright Copyright (c) MISTER.AM, LLC
+ * @author    Liuba Kalyta <kalyta@dotsplatform.com>
+ */
+
+namespace KalytaTest\Package\App;
+
+class Test
+{
+
+}
